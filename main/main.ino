9abcc1f3a92f7f46b08c6cda1f3403eaece726cb
Gael Ziade <rgaelziade@gmail.com>
2021-09-10 17:42:08 -0300

    inclusion de lectura de temperatura

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,23 @@
 #include "LiquidCrystal.h"
+#include "AnalogSensor.h"
 
-void setup() {
-  
+AnalogSensor LM335 = AnalogSensor();
+
+void setup(){
+  LM335.Config(A0, 558.558, 763.158, 0, 100);
+  Serial.begin(9600);
 
 }
 
-void loop() {
-  
 
+void loop(){
+  float temperatura;
+
+  temperatura = LM335.Leer();
+
+  Serial.println("--------------------------------");
+  Serial.println("Temp: " + String(temperatura) + " *C");
+
+  delay(500);
+  
 }
