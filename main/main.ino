9b8c066a95a3eb2bd9f5a4646fb48aafc115a263
Gael Ziade <rgaelziade@gmail.com>
2021-10-13 17:05:38 -0300

    notas del profe

--- main/main.ino
+++ main/main.ino
@@ -3,6 +3,11 @@
 dht DHT;
 #define DHT11_PIN 8
 
+// Funcion de escritura desde el NodeMCU
+// Almacenamiento d datos no volatiles
+// RTC Arduino DS1307/3231 "Luis Llamas"
+// Banda Proporcional
+
 byte lcd0 = 6;
 byte lcd1 = 5;
 byte lcd2 = 4;
@@ -10,16 +15,16 @@ byte lcd3 = 13;
 byte lcdrs = 7;
 byte lcdenable = 9;
 
-LiquidCrystal lcd (lcdrs, lcdenable, lcd0, lcd1, lcd2, lcd3);
+LiquidCrystal lcd(lcdrs, lcdenable, lcd0, lcd1, lcd2, lcd3);
 
 byte celsius[8] = {
-  B00111,
-  B00101,
-  B00111,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
+    B00111,
+    B00101,
+    B00111,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
 };
 
 float currenttemperatura;
@@ -31,7 +36,8 @@ float humedadset;
 boolean overtemp;
 boolean overhumedad;
 
-void setup() {
+void setup()
+{
 
   Serial.begin(9600);
 
@@ -41,49 +47,50 @@ void setup() {
 
   tempset = 31;
   humedadset = 20;
-
 }
 
-
-void loop() {
+void loop()
+{
 
   DHT.read11(DHT11_PIN);
   currenthumedad = DHT.humidity;
   currenttemperatura = DHT.temperature;
 
-  lcd.print ("Temperatura:");
-  lcd.print (String(DHT.temperature));
-  lcd.print (" C");
+  lcd.print("Temperatura:");
+  lcd.print(String(DHT.temperature));
+  lcd.print(" C");
   lcd.write(byte(0));
 
-  lcd.setCursor(0,2);
+  lcd.setCursor(0, 2);
   Serial.println(currenthumedad);
-  lcd.print ("Humedad:");
-  lcd.print (String(DHT.humidity));
-  lcd.print ("%");
+  lcd.print("Humedad:");
+  lcd.print(String(DHT.humidity));
+  lcd.print("%");
   lcd.home();
 
-
-  if (currenttemperatura > tempset) {
+  if (currenttemperatura > tempset)
+  {
     overtemp = true;
     digitalWrite(13, HIGH);
   }
 
-  else {
+  else
+  {
     overtemp = false;
     digitalWrite(13, LOW);
   }
 
-    if (currenthumedad > humedadset) {
+  if (currenthumedad > humedadset)
+  {
     overhumedad = true;
     Serial.println("AAAAAAH QUE HUMEDAD!!");
   }
 
-  else {
+  else
+  {
     overtemp = false;
     Serial.println("AAAAAAH QUE HUMEDAN'T!!");
   }
 
   delay(100);
-
 }
