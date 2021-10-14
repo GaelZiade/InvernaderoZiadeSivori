6e6e8f760375fa40efe15c18472db9f49faa6c75
Gael Ziade <rgaelziade@gmail.com>
2021-10-14 10:41:55 -0300

    RTC agregado (WIP)

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,7 @@
 #include "LiquidCrystal.h"
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS1307 rtc;
 #include "dht.h"
 dht DHT;
 #define DHT11_PIN 8
@@ -7,6 +10,31 @@ dht DHT;
 // Almacenamiento d datos no volatiles
 // RTC Arduino DS1307/3231 "Luis Llamas"
 // Banda Proporcional
+
+//DELCARACION VARIABLES RTC
+// Fijar a fecha y hora específica. En el ejemplo, 21 de Enero de 2016 a las 03:00:00
+// rtc.adjust(DateTime(2016, 1, 21, 3, 0, 0));
+String daysOfTheWeek[7] = {"Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
+String monthsNames[12] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
+//DECLARACION DE FUNCION PARA IMRESION DE FECHA
+void printDate(DateTime date)
+{
+  Serial.print(date.year(), DEC);
+  Serial.print('/');
+  Serial.print(date.month(), DEC);
+  Serial.print('/');
+  Serial.print(date.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[date.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(date.hour(), DEC);
+  Serial.print(':');
+  Serial.print(date.minute(), DEC);
+  Serial.print(':');
+  Serial.print(date.second(), DEC);
+  Serial.println();
+}
 
 //DELCARACION PINES LCD
 byte lcd0 = 6;
@@ -47,6 +75,18 @@ void setup()
 
   Serial.begin(9600);
 
+  if (!rtc.begin())
+  {
+    Serial.println(F("Couldn't find RTC"));
+    while (1)
+      ;
+  }
+
+  if (rtc.lostPower())
+  {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
   lcd.createChar(0, celsius);
   lcd.begin(20, 4);
   lcd.home();
@@ -55,11 +95,13 @@ void setup()
   humedadset = 60;
   margentemp = 5;
   margenhumedad = 5;
-
 }
 
 void loop()
 {
+
+  DateTime now = rtc.now();
+  printDate(now);
 
   //LEER VALORES DEL SENSOR
   DHT.read11(DHT11_PIN);
@@ -80,25 +122,29 @@ void loop()
   lcd.home();
 
   //ENCENDIDO/APAGADO DE TEMPERATURA CON MARGEN
-  if (currenttemperatura >= (tempset + margentemp)){
+  if (currenttemperatura >= (tempset + margentemp))
+  {
     tempstate = true;
-    Serial.println("Temperatura = " + String(currenttemperatura) + ": HIGH");
+    //Serial.println("Temperatura = " + String(currenttemperatura) + ": HIGH");
   }
 
-  if (currenttemperatura <= (tempset - margentemp)){
+  if (currenttemperatura <= (tempset - margentemp))
+  {
     tempstate = false;
-    Serial.println("Temperatura = " + String(currenttemperatura) + ": LOW");
+    //Serial.println("Temperatura = " + String(currenttemperatura) + ": LOW");
   }
 
   //ENCENDIDO/APAGADO DE HUMEDAD CON MARGEN
-  if (currenthumedad >= (humedadset + margenhumedad)){
+  if (currenthumedad >= (humedadset + margenhumedad))
+  {
     humedadstate = true;
-    Serial.println("Humedad = " + String(currenthumedad) + ": HIGH");
+    //Serial.println("Humedad = " + String(currenthumedad) + ": HIGH");
   }
 
-  if (currenthumedad <= (humedadset - margenhumedad)){
+  if (currenthumedad <= (humedadset - margenhumedad))
+  {
     humedadstate = false;
-    Serial.println("Humedad = " + String(currenthumedad) + ": LOW");
+    //Serial.println("Humedad = " + String(currenthumedad) + ": LOW");
   }
 
   delay(100);
